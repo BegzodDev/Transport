5a7b13f0ba22b227739db65fb66912e8e8d9ddf5
asliddin <user@WIN-QBJNHEE9ROF>
2023-03-11 14:02:35 +0500

    refactoring

---
+++ src/Transport.Application/UseCase/User/Commands/Trains/CreateTrainTicketCommand.cs
@@ -0,0 +1,196 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Transport.Application.Abstractions;
+using Transport.Application.Exceptions;
+using Transport.Domain.Entities;
+using Transport.Domain.Enums;
+
+namespace Transport.Application.UseCase.User.Commands
+{
+    public class CreateTrainTicketCommand : ICommand<Unit>
+    {
+        [Required]
+
+        public string? PasportSeries { get; set; }
+        [Required]
+
+        public DateOnly? Date { get; set; }
+        [Required]
+
+        public string? From { get; set; }
+        [Required]
+
+        public string? For { get; set; }
+        [Required]
+
+        public int? Place { get; set; }
+        [Required]
+
+        public Status Status { get; set; }
+        [Required]
+
+    }
+
+    public class CreateTrainTicketCommandHandler : ICommandHandler<CreateTrainTicketCommand, Unit>
+    {
+        //Interfaces
+        private readonly IApplicationDbContext _context;
+        private readonly IGovermentService _govermentService;
+        private readonly ISecurityService _securityService;
+        private readonly IEconomyService _economyService;
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IDistributedCache _distrubuteCache;
+
+        //Constructor
+
+        public CreateTrainTicketCommandHandler(IApplicationDbContext context,
+                                                IGovermentService govermentService,
+                                                ISecurityService securityService,
+                                                IEconomyService economyService,
+                                                ICurrentUserService currentUserService,
+                                                IDistributedCache distributedCache)
+        {
+            _securityService = securityService;
+            _context = context;
+            _govermentService = govermentService;
+            _economyService = economyService;
+            _currentUserService = currentUserService;
+            _distrubuteCache = distributedCache;
+        }
+
+        public async Task<Unit> Handle(CreateTrainTicketCommand command, CancellationToken cancellationToken)
+        {
+            if (!_govermentService.Check(command.PasportSeries!))
+            {
+                throw new Exception("Pasport series is don't have in our base");
+            }
+            _securityService.CheckSecure(command.PasportSeries!);
+            var reys = _context.airlines.FirstOrDefault(x => x.Flight_From!.ToLower() == command.From!.ToLower() &&
+                                                        x.Flight_For!.ToLower() == command.For!.ToLower() &&
+                                                        x.Date == command.Date);
+
+            if(reys = null)
+            {
+                throw new TrainNotFoundException();
+            }
+            if(command.Status = Status.Econom)
+            {
+                foreach (var places in _context.placeTrains)
+                {
+                    if (places.Place_in_Ticket == command.Place && places.Status == Status.Econom)
+                    {
+                        throw new Exception("This place is busy");
+                    }
+                    continue;
+                }
+            }
+            if(command.Status = Status.Buiseness)
+            {
+                foreach (var places in _context.placeTrains)
+                {
+                    if (places.Place_in_Ticket == command.Place && places.Status == Status.Buiseness)
+                    {
+                        throw new Exception("This place is busy");
+                    }
+                    continue;
+                }
+            }
+            if (command.Status = Status.VIP)
+            {
+                foreach (var places in _context.placeTrains)
+                {
+                    if (places.Place_in_Ticket == command.Place && places.Status == Status.VIP)
+                    {
+                        throw new Exception("This place is busy");
+                    }
+                    continue;
+                }
+            }
+            var place = new PlaceAirline
+            {
+                Status = command.Status,
+                Place_in_Ticket = command.Place,
+                AirlineId = reys.Id
+            };
+
+            var user = _context.users.FirstOrDefault(x => x.UserName == command.From);
+
+            var tickets = new TicketAirline();
+
+            if(!_economyService.PaymentCheck(command.PasportSeries!, (double)reys.Price))
+            {
+                throw new Exception("Payment si valid");
+            }
+            else if(command.Status = Status.Econom)
+            {
+                if(_economyService.PaymentCheck(command.PasportSeries, (double)reys.Price))
+                {
+                    tickets = new TicketAirline()
+                    {
+                        UserId = reys.Id,
+                        PlaceAirlineId = place.Id,
+                        From = reys.Flight_From,
+                        For = reys.Flight_For,
+                        dateTime = reys.Date,
+                        PasportSeries = command.PasportSeries
+                    };
+                }
+                else
+                {
+                    throw new Exception("Invalid pasport or not enough money")
+                }
+            }
+            else if (command.Status = Status.Buiseness)
+            {
+                if (_economyService.PaymentCheck(command.PasportSeries, (double)reys.Price))
+                {
+                    tickets = new TicketAirline()
+                    {
+                        UserId = reys.Id,
+                        PlaceAirlineId = place.Id,
+                        From = reys.Flight_From,
+                        For = reys.Flight_For,
+                        dateTime = reys.Date,
+                        PasportSeries = command.PasportSeries
+                    };
+                }
+                else
+                {
+                    throw new Exception("Invalid pasport or not enough money")
+                }
+            }
+            else if (command.Status = Status.VIP)
+            {
+                if (_economyService.PaymentCheck(command.PasportSeries, (double)reys.Price))
+                {
+                    tickets = new TicketAirline()
+                    {
+                        UserId = reys.Id,
+                        PlaceAirlineId = place.Id,
+                        From = reys.Flight_From,
+                        For = reys.Flight_For,
+                        dateTime = reys.Date,
+                        PasportSeries = command.PasportSeries
+                    };
+                }
+                else
+                {
+                    throw new Exception("Invalid pasport or not enough money")
+                }
+            }
+
+            await _context.ticketAirlines.AddAsync(tickets);
+            await _context.SaveChangesAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+
+
+}
---
+++ src/Transport.Infrastructure/Migrations/20230305102812_init.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Transport.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "airlines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Flight_From = table.Column<string>(type: "text", nullable: true),
+                    Flight_For = table.Column<string>(type: "text", nullable: true),
+                    Price = table.Column<double>(type: "double precision", nullable: true),
+                    Date = table.Column<DateOnly>(type: "date", nullable: true),
+                    Count_Business_Class_Place = table.Column<int>(type: "integer", nullable: true),
+                    Count_Econom_Class_Place = table.Column<int>(type: "integer", nullable: true),
+                    Count_VIP_Class_Place = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_airlines", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "bus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    From = table.Column<string>(type: "text", nullable: true),
+                    For = table.Column<string>(type: "text", nullable: true),
+                    Price = table.Column<double>(type: "double precision", nullable: true),
+                    OrderForBusId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_bus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "trains",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    From = table.Column<string>(type: "text", nullable: true),
+                    For = table.Column<string>(type: "text", nullable: true),
+                    Price = table.Column<double>(type: "double precision", nullable: true),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Count_Business_Class_Place = table.Column<int>(type: "integer", nullable: true),
+                    Count_Econom_Class_Place = table.Column<int>(type: "integer", nullable: true),
+                    Count_VIP_Class_Place = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_trains", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserName = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "placeAirlines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Place_in_Ticket = table.Column<int>(type: "integer", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    AirlineId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_placeAirlines", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_placeAirlines_airlines_AirlineId",
+                        column: x => x.AirlineId,
+                        principalTable: "airlines",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "placeTrains",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Place_in_Ticket = table.Column<int>(type: "integer", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    TrainId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_placeTrains", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_placeTrains_trains_TrainId",
+                        column: x => x.TrainId,
+                        principalTable: "trains",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Admin",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admin", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Admin_users_Id",
+                        column: x => x.Id,
+                        principalTable: "users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ticketBuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Sum = table.Column<double>(type: "double precision", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: true),
+                    BusId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ticketBuses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ticketBuses_bus_BusId",
+                        column: x => x.BusId,
+                        principalTable: "bus",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ticketBuses_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ticketAirlines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    dateTime = table.Column<DateOnly>(type: "date", nullable: true),
+                    From = table.Column<string>(type: "text", nullable: true),
+                    For = table.Column<string>(type: "text", nullable: true),
+                    PasportSeries = table.Column<string>(type: "text", nullable: true),
+                    UserId = table.Column<int>(type: "integer", nullable: true),
+                    PlaceAirlineId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ticketAirlines", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ticketAirlines_placeAirlines_PlaceAirlineId",
+                        column: x => x.PlaceAirlineId,
+                        principalTable: "placeAirlines",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ticketAirlines_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ticketTrains",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    dateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PassergerForTrainId = table.Column<int>(type: "integer", nullable: true),
+                    UserId = table.Column<int>(type: "integer", nullable: true),
+                    PlaceTrainId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ticketTrains", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ticketTrains_placeTrains_PlaceTrainId",
+                        column: x => x.PlaceTrainId,
+                        principalTable: "placeTrains",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ticketTrains_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_placeAirlines_AirlineId",
+                table: "placeAirlines",
+                column: "AirlineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_placeTrains_TrainId",
+                table: "placeTrains",
+                column: "TrainId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ticketAirlines_PlaceAirlineId",
+                table: "ticketAirlines",
+                column: "PlaceAirlineId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ticketAirlines_UserId",
+                table: "ticketAirlines",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ticketBuses_BusId",
+                table: "ticketBuses",
+                column: "BusId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ticketBuses_UserId",
+                table: "ticketBuses",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ticketTrains_PlaceTrainId",
+                table: "ticketTrains",
+                column: "PlaceTrainId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ticketTrains_UserId",
+                table: "ticketTrains",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admin");
+
+            migrationBuilder.DropTable(
+                name: "ticketAirlines");
+
+            migrationBuilder.DropTable(
+                name: "ticketBuses");
+
+            migrationBuilder.DropTable(
+                name: "ticketTrains");
+
+            migrationBuilder.DropTable(
+                name: "placeAirlines");
+
+            migrationBuilder.DropTable(
+                name: "bus");
+
+            migrationBuilder.DropTable(
+                name: "placeTrains");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "airlines");
+
+            migrationBuilder.DropTable(
+                name: "trains");
+        }
+    }
+}
