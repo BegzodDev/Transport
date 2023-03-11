c94d0d0048e4c48a61ee7c0f91d10f67600c9b9d
alimjanovf <almjnvfsystem@gmail.com>
2023-03-11 14:26:20 +0500

    Bus crud implemented

---
+++ src/Transport.Application/UseCase/User/Commands/UpdateBusTicketCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Abstractions;
+
+namespace Transport.Application.UseCase.User.Commands
+{
+    public  class UpdateBusTicketCommand : ICommand<Unit>
+    {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+    }
+
+    public class UpdateBusTicketCommandHandler : ICommandHandler<UpdateBusTicketCommand, Unit>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateBusTicketCommandHandler(IApplicationDbContext context)
+        {
+            _context = context; 
+        }
+
+        public async Task<Unit> Handle(UpdateBusTicketCommand request, CancellationToken cancellationToken)
+        {
+            var ticket = await _context.ticketBuses.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+            if (ticket == null)
+            {
+                throw new Exception("Ticket not found");
+            }
+
+            ticket.Date = request.Date.ToDateTime(TimeOnly.MinValue);
+            _context.ticketBuses.Update(ticket);
+
+            return Unit.Value;
+        }
+    }
+}
--- src/Transport/Controllers/BusController.cs
+++ src/Transport/Controllers/BusController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Transport.Application.UseCase.User.Commands;
+using Transport.Application.UseCase.User.Queries;
 
 namespace Transport.Api.Controllers
 {
@@ -18,6 +19,30 @@ namespace Transport.Api.Controllers
         public async Task<IActionResult> Create(CreateBussTicketCommand command)
         {
             await _mediator.Send(command);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateBusTicketCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(DeleteBusTicketCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(GetBusTicketQuery query)
+        {
+            await _mediator.Send(query);
 
             return Ok();
         }
